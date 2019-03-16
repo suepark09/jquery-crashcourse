bce65adb361b6cf2414cad300d2c5a84bb052a1c
Sue Park <suepandrice06@gmail.com>
2019-03-16 14:47:45 -0500

    part one JS review dunzooo

--- power.js
+++ power.js
@@ -11,4 +11,134 @@ var iamstring = 'i am string';
 var iamnumber = 123;
 var alsoastring = '123';
 
-console.log(myFirstVariable, mySecondVariable, alsoastring);
+// console.log(myFirstVariable, mySecondVariable, alsoastring);
+
+
+//Lists 
+
+var firstArray = [
+'one',
+'two',
+'three'
+];
+
+// console.log(firstArrray);
+// console.log(firstArray.push('four'));
+// console.log(firstArray);
+
+// var secondArray = [];
+
+// console.log(secondArray);
+
+firstArray[1] = 'changed u';
+// console.log(firstArray);
+
+
+//OBJECTS!!!!!   /key/ value stores
+
+var firstObject = {
+    key1: 'Key 1 Value',
+    key2: 'Key 2 Value',
+    key3: 'Key 3 Value'
+};
+
+// console.log(firstObject);
+firstObject.key3 = 'wutever i wantttt';
+// console.log(firstObject.key3, firstObject.key1);
+
+var noob1 = {
+    name: 'nick',
+    hair: 'brown'
+}
+
+var noob2 = {};
+noob2.name = 'chris';
+noob2.hair = 'black';
+
+var noobs = [
+noob1,
+noob2
+];
+
+// console.log(noobs);
+
+var noobs = {
+    'admin': noob1,
+    'dev': noob2,
+    'objectDemo': {
+        key1: 'Key 1 Value',
+        key2: 'Key 2 Value',
+        key3: 'Key 3 Value'
+    },
+    'arrayDemo': [
+        'one',
+        'two',
+        'three'
+        ]
+};
+
+// console.log(noobs);
+
+//OUTPUTTING JS
+
+var outDemo = 'the lion king';
+
+//debug way
+// console.log('outDemo');
+
+// document.write('outDemo');
+
+
+//multiple var declarations
+
+var item1 ='yuhh',
+    item2 = 'yehhh2',
+    item3 = 'yuhhh3';
+
+    // built in methods and functions
+var sky = 'blue';
+sky = sky.toUpperCase();
+sky = sky.charAt(0);
+// document.write(sky);
+
+//conditionals ---> logic with JS
+
+var amount = -200;
+var count = 30;
+
+// if(amount <= count) {
+//     console.log('it matches');
+// } else {
+//     console.log('u dumb');
+// }
+
+
+
+var myString = 'hello';
+myString = 'usuckk'
+
+if('hello' == myString) {
+    console.log('it matches');
+} else {
+    console.log('u dumb');
+}
+
+//Global Scope
+
+function demo1() {
+
+}
+
+var airplanes = 'nothing is above meeee'
+function earth() {
+    var molepeople = '**i belong in this function**';
+}
+earth(); 
+console.log(airplanes, molepeople);
+
+function demo3() {
+
+    function demo4() {
+    
+    }
+}
