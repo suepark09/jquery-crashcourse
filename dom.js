cde08c0af90655044657a9451dd0d16c29b13a4d
Sue Park <suepandrice06@gmail.com>
2019-03-17 12:36:17 -0500

    DOM manipulation

---
+++ dom.js
@@ -0,0 +1,11 @@
+
+//waiting for HTML to run first before executing JS 
+// $(document).ready(function() {
+
+
+// });
+
+//Shorthand for above code
+$(function() {
+    $('body').css('background', 'blue');
+});
