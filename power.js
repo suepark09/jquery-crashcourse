e9dc423a2dfd55b9dbebe92c633cafeb03485eb8
Sue Park <suepandrice06@gmail.com>
2019-03-16 20:02:30 -0500

    first jquery

--- power.js
+++ power.js
@@ -1,144 +1,154 @@
-// variable declaration
-var myFirstVariable;
-
-myFirstVariable = 'wutever i wantt';
-
-var mySecondVariable = 'Totally cool dude';
-
-// strings and numbers
-
-var iamstring = 'i am string';
-var iamnumber = 123;
-var alsoastring = '123';
-
-// console.log(myFirstVariable, mySecondVariable, alsoastring);
+//My first jQuery
+$('#make-me-red').css({
+    color: 'red'
+});
 
 
-//Lists 
 
-var firstArray = [
-'one',
-'two',
-'three'
-];
-
-// console.log(firstArrray);
-// console.log(firstArray.push('four'));
-// console.log(firstArray);
-
-// var secondArray = [];
-
-// console.log(secondArray);
-
-firstArray[1] = 'changed u';
-// console.log(firstArray);
+//NOTES HERE
 
 
-//OBJECTS!!!!!   /key/ value stores
+// // variable declaration
+// var myFirstVariable;
 
-var firstObject = {
-    key1: 'Key 1 Value',
-    key2: 'Key 2 Value',
-    key3: 'Key 3 Value'
-};
+// myFirstVariable = 'wutever i wantt';
 
-// console.log(firstObject);
-firstObject.key3 = 'wutever i wantttt';
-// console.log(firstObject.key3, firstObject.key1);
+// var mySecondVariable = 'Totally cool dude';
 
-var noob1 = {
-    name: 'nick',
-    hair: 'brown'
-}
+// // strings and numbers
 
-var noob2 = {};
-noob2.name = 'chris';
-noob2.hair = 'black';
+// var iamstring = 'i am string';
+// var iamnumber = 123;
+// var alsoastring = '123';
 
-var noobs = [
-noob1,
-noob2
-];
-
-// console.log(noobs);
-
-var noobs = {
-    'admin': noob1,
-    'dev': noob2,
-    'objectDemo': {
-        key1: 'Key 1 Value',
-        key2: 'Key 2 Value',
-        key3: 'Key 3 Value'
-    },
-    'arrayDemo': [
-        'one',
-        'two',
-        'three'
-        ]
-};
-
-// console.log(noobs);
-
-//OUTPUTTING JS
-
-var outDemo = 'the lion king';
-
-//debug way
-// console.log('outDemo');
-
-// document.write('outDemo');
+// // console.log(myFirstVariable, mySecondVariable, alsoastring);
 
 
-//multiple var declarations
+// //Lists 
 
-var item1 ='yuhh',
-    item2 = 'yehhh2',
-    item3 = 'yuhhh3';
+// var firstArray = [
+// 'one',
+// 'two',
+// 'three'
+// ];
 
-    // built in methods and functions
-var sky = 'blue';
-sky = sky.toUpperCase();
-sky = sky.charAt(0);
-// document.write(sky);
+// // console.log(firstArrray);
+// // console.log(firstArray.push('four'));
+// // console.log(firstArray);
 
-//conditionals ---> logic with JS
+// // var secondArray = [];
 
-var amount = -200;
-var count = 30;
+// // console.log(secondArray);
 
-// if(amount <= count) {
+// firstArray[1] = 'changed u';
+// // console.log(firstArray);
+
+
+// //OBJECTS!!!!!   /key/ value stores
+
+// var firstObject = {
+//     key1: 'Key 1 Value',
+//     key2: 'Key 2 Value',
+//     key3: 'Key 3 Value'
+// };
+
+// // console.log(firstObject);
+// firstObject.key3 = 'wutever i wantttt';
+// // console.log(firstObject.key3, firstObject.key1);
+
+// var noob1 = {
+//     name: 'nick',
+//     hair: 'brown'
+// }
+
+// var noob2 = {};
+// noob2.name = 'chris';
+// noob2.hair = 'black';
+
+// var noobs = [
+// noob1,
+// noob2
+// ];
+
+// // console.log(noobs);
+
+// var noobs = {
+//     'admin': noob1,
+//     'dev': noob2,
+//     'objectDemo': {
+//         key1: 'Key 1 Value',
+//         key2: 'Key 2 Value',
+//         key3: 'Key 3 Value'
+//     },
+//     'arrayDemo': [
+//         'one',
+//         'two',
+//         'three'
+//         ]
+// };
+
+// // console.log(noobs);
+
+// //OUTPUTTING JS
+
+// var outDemo = 'the lion king';
+
+// //debug way
+// // console.log('outDemo');
+
+// // document.write('outDemo');
+
+
+// //multiple var declarations
+
+// var item1 ='yuhh',
+//     item2 = 'yehhh2',
+//     item3 = 'yuhhh3';
+
+//     // built in methods and functions
+// var sky = 'blue';
+// sky = sky.toUpperCase();
+// sky = sky.charAt(0);
+// // document.write(sky);
+
+// //conditionals ---> logic with JS
+
+// var amount = -200;
+// var count = 30;
+
+// // if(amount <= count) {
+// //     console.log('it matches');
+// // } else {
+// //     console.log('u dumb');
+// // }
+
+
+
+// var myString = 'hello';
+// myString = 'usuckk'
+
+// if('hello' == myString) {
 //     console.log('it matches');
 // } else {
 //     console.log('u dumb');
 // }
 
+// //Global Scope
 
+// function demo1() {
 
-var myString = 'hello';
-myString = 'usuckk'
+// }
 
-if('hello' == myString) {
-    console.log('it matches');
-} else {
-    console.log('u dumb');
-}
+// var airplanes = 'nothing is above meeee'
+// function earth() {
+//     var molepeople = '**i belong in this function**';
+// }
+// earth(); 
+// console.log(airplanes, molepeople);
 
-//Global Scope
+// function demo3() {
 
-function demo1() {
-
-}
-
-var airplanes = 'nothing is above meeee'
-function earth() {
-    var molepeople = '**i belong in this function**';
-}
-earth(); 
-console.log(airplanes, molepeople);
-
-function demo3() {
-
-    function demo4() {
+//     function demo4() {
     
-    }
-}
+//     }
+// }
