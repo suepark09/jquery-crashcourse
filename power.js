bc0549567f2b7401bf88393622582316bac94edb
Sue Park <suepandrice06@gmail.com>
2019-03-18 07:02:14 -0500

    Events section

--- power.js
+++ power.js
@@ -1,4 +1,13 @@
 $(function() {
-   $('#second-one').addClass('super-bold');
+
+    $('h2').on('click', function() {
+        var $this = $(this);
+        console.log($this.next().text());
     
+    });
+
+    // $('h2').click(function() {
+    //     console.log('u clicked on h2');
+    // });
+
 });
